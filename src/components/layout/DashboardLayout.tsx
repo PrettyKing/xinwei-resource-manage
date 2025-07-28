2af4e9f5a165fefcd00bfdc3461a0810b81aea00
chalee <695469701@qq.com>
2025-07-28 10:37:41 +0800

    feat: update

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -188,29 +188,10 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
               >
                 <MenuIcon size={24} />
               </button>
-              
-              {/* 搜索框 */}
-              <div className="relative hidden md:block">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <SearchIcon size={20} className="text-gray-400" />
-                </div>
-                <input
-                  type="text"
-                  placeholder="搜索..."
-                  className="pl-10 pr-4 py-2 w-64 bg-gray-50 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                />
-              </div>
             </div>
 
             {/* 右侧 */}
             <div className="flex items-center space-x-4">
-              {/* 通知按钮 */}
-              <button className="p-2 rounded-lg hover:bg-gray-100 transition-colors duration-200 relative">
-                <BellIcon size={20} />
-                <span className="absolute -top-1 -right-1 w-4 h-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
-                  3
-                </span>
-              </button>
 
               {/* 用户菜单 */}
               <div className="relative" ref={userMenuRef}>
